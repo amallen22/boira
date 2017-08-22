bd86b98f33839205d100605521df4ae11bb36c54
Alex Mallen <alex.mallengines@gmail.com>
2017-08-22 17:51:00 +0200

    Adding basic structure for Boira App. Use Bower, Express and node modules: path and express

---
+++ app.js
@@ -0,0 +1,10 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const PORT = 3022
+
+const pathPublic = path.join(process.cwd(), 'client')
+app.use(express.static(pathPublic))
+
+app.listen(PORT)
+console.log('Run in port ' + PORT)
