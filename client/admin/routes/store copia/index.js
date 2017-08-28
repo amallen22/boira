507bda769995a817996b768f31a2f64aaacdbc83
Alex Mallen <alex.mallengines@gmail.com>
2017-08-28 22:21:13 +0200

    Apply filter by category in store products (snippet)

---
+++ client/admin/routes/store copia/index.js
@@ -0,0 +1,8 @@
+angular.module('boiraApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+  .when('/store', {
+    templateUrl: 'routes/store/template.html',
+    controller: 'storeController'
+  })
+  })
--- client/admin/routes/store/storeController.js
+++ client/admin/routes/store/storeController.js
@@ -1,25 +1,45 @@
 angular.module('boiraApp')
 
-.controller('storeController', function ($scope, $rootScope, localData) {
+.controller('storeController', function ($scope, $rootScope, localData, $q) {
   $scope.section = 'STORE'
+
+  // Local API item store
   localData.storeData()
   .then(function (response) {
-    console.log(response.data.products)
+    // console.log(response.data.products[0].category.slug)
     $scope.products = response.data.products
-
-    $scope.btnCategories = ['T-shirts', 'Bags', 'Music']
   })
-  // $scope.localData = localData
-})
-.filter('myFilter', function () {
-  return function (items, param1, param2) {
-    return items.filter(function (item) { /* your custom conditions */ })
+
+  // define buttons
+  $scope.categories = ['tshirts', 'bags', 'music']
+
+  // get value form buttons
+  $scope.setSelectedTitle = function (value) {
+    if ($scope.selectedCategory === value) {
+      $scope.selectedCategory = undefined
+    } else {
+      $scope.selectedCategory = value
+    }
   }
+
+  // Filter
+  $scope.byCategory = function (entry) {
+    return entry.category.slug === $scope.selectedCategory || $scope.selectedCategory === undefined
+  }
+  // load data
+  // $q.when({ data: [
+  //       { category: 'tshirts', text: 1 },
+  //       { category: 'bags', text: 2 },
+  //       { category: 'tshirts', text: 3 },
+  //       { category: 'music', text: 4 },
+  //       { category: 'tshirts', text: 5 },
+  //       { category: 'bags', text: 6 },
+  //       { category: 'bags', text: 7 },
+  //       { category: 'music', text: 8 }
+  // ]})
+
+  // promise
+    // .then(function (res) {
+    //   $scope.entries = res.data
+    // })
 })
-// .controller('homeController', function ($scope, dataService) {
-//   dataService.getRestaurant()
-//     .then(function (response) {
-//       $scope.restaurants = response.data.results
-//       console.log(response.data)
-//     })
-// })
